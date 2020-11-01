a48a9552c6266ad2749f429ff1e1658d584e2053
chuxmykel <ngwobiachukwudi@gmail.com>
2020-11-02 00:11:45 +0100

    implement stack

--- src/Stack/index.ts
+++ src/Stack/index.ts
@@ -1,3 +1,28 @@
-export class Stack {
+class Node {
+  public next: Node | null = null;
+  constructor(public data: number) {}
+}
 
+export class Stack {
+  private top: Node | null = null;
+
+  get isEmpty(): boolean { return this.top === null }
+
+  public peek(): number {
+    if (this.top === null) { throw new Error('Stack is empty') }
+    return this.top.data;
+  }
+
+  public push(data: number): void {
+    const node = new Node(data);
+    node.next = this.top;
+    this.top = node;
+  }
+
+  public pop(): number {
+    if (this.top === null) { throw new Error('Stack is empty') }
+    const { data } = this.top;
+    this.top = this.top.next;
+    return data;
+  }
 }
